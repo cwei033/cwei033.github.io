2ee394179a627069a0b9115a34a3ade358b50108
cwei033 <60452220+cwei033@users.noreply.github.com>
2021-03-05 23:32:58 -0800

    new

--- portfolio/sketch.js
+++ portfolio/sketch.js
@@ -1,35 +1,7 @@
-let column = [];
-let columncalc = 2000;
-let columnmove;
-let stripe = [0,40];
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-
-
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background('#CFC096');
-
-  columncalc -= 80;
-  column.push(columncalc);
-  for (let i = 0; i < column.length; i++) {
-    stripe[i] = new Stripe(column[i] += 1, 'rgba(223, 47, 2, 1)', 1); }
-
-
-  console.log(column);
-
-  for (let i = 0; i < column.length; i++) {
-    stripe[i].display();
-    // stripe[i].move();
-
-    // if (stripe[i].x >= windowWidth) {
-    //   stripe[i].x = column[0];
-    // }
-  }
-
-
-
+  background(220);
 }
